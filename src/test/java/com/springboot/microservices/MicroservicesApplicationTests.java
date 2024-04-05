91f6bdd7ccd710098b1fc7596feee164e9830596
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-04-05 21:58:47 +0530

    feature service registry implemented

---
+++ src/test/java/com/springboot/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
