b47122f144e5e3c15fd25c402cee7fbbcfd87285
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-04-09 01:47:03 +0530

    api gateway implemented

--- department-service/src/main/java/com/springboot/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/springboot/departmentservice/controller/DepartmentController.java
@@ -1,5 +1,6 @@
 package com.springboot.departmentservice.controller;
 
+import com.springboot.departmentservice.client.EmployeeClient;
 import com.springboot.departmentservice.model.Department;
 import com.springboot.departmentservice.repository.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +11,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/department")
+@RequestMapping("/api/v1/department")
 @RequiredArgsConstructor
 public class DepartmentController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
 
     private final DepartmentRepository repository;
+
+    private final EmployeeClient employeeClient;
 
     @PostMapping
     public Department add(@RequestBody Department department){
@@ -34,5 +37,16 @@ public class DepartmentController {
     public Department findById(@PathVariable("id") Long id){
         LOGGER.info("Department find: id={}", id);
         return repository.findById(id);
+    }
+
+    @GetMapping("/with-employees")
+    public List<Department> findAllWthEmployees(){
+        LOGGER.info("Department find");
+        List<Department> departments = repository.findAll();
+        departments.forEach(department ->
+                department.setEmployees(
+                        employeeClient.findByDepartment(department.getId())));
+
+        return departments;
     }
 }
