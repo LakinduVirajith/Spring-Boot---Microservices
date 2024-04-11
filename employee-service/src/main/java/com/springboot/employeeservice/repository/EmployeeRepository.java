5649d2ed74cfab3f1cc7f1c79b262ecabf7404ab
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-04-11 13:28:38 +0530

    ci-cd pipelines implemented

--- employee-service/src/main/java/com/springboot/employeeservice/repository/EmployeeRepository.java
+++ employee-service/src/main/java/com/springboot/employeeservice/repository/EmployeeRepository.java
@@ -2,10 +2,8 @@ package com.springboot.employeeservice.repository;
 
 import com.springboot.employeeservice.collection.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
