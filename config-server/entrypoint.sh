330115085ab6958eec39d14f0fd83e3f829fdcbb
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-04-12 17:29:45 +0530

    setup env

--- config-server/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-if [ -z "${MONGODB_USERNAME}" ] || [ -z "${MONGODB_PASSWORD}" ]; then
-  echo "Error: Missing required environment variables. Please set MONGODB_USERNAME and MONGODB_PASSWORD."
-  exit 1
-fi
-
-# Run your config-server application here
-exec java -jar your-config-server.jar
