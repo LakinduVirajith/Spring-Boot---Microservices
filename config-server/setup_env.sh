a4ff988227000fa1a07b721a2691f8fde1a5f1ed
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-04-12 20:19:39 +0530

    test

--- config-server/setup_env.sh
+++ config-server/setup_env.sh
@@ -7,3 +7,11 @@ sed -i "s/\${MONGODB_PASSWORD}/$MONGODB_PASSWORD/g" src/main/resources/config/de
 # Replace placeholders in Employee service yaml
 sed -i "s/\${MONGODB_USERNAME}/$MONGODB_USERNAME/g" src/main/resources/config/employee-service.yaml
 sed -i "s/\${MONGODB_PASSWORD}/$MONGODB_PASSWORD/g" src/main/resources/config/employee-service.yaml
+
+# Display Department service yaml with passwords
+echo "Department Service YAML with passwords:"
+cat src/main/resources/config/department-service.yaml
+
+# Display Employee service yaml with passwords
+echo "Employee Service YAML with passwords:"
+cat src/main/resources/config/employee-service.yaml
