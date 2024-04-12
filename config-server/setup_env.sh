c83902bd1b213274e002597b308c24eae0e65ee5
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-04-12 20:29:52 +0530

    test

--- config-server/setup_env.sh
+++ config-server/setup_env.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Display Department service yaml with passwords
-echo "Department Service YAML with passwords:"
-cat src/main/resources/config/department-service.yaml
-
-# Display Employee service yaml with passwords
-echo "Employee Service YAML with passwords:"
-cat src/main/resources/config/employee-service.yaml
-
-# Replace placeholders in Department service yaml
-sed -i "s/\${MONGODB_USERNAME}/$MONGODB_USERNAME/g" src/main/resources/config/department-service.yaml
-sed -i "s/\${MONGODB_PASSWORD}/$MONGODB_PASSWORD/g" src/main/resources/config/department-service.yaml
-
 # Replace placeholders in Employee service yaml
 sed -i "s/\${MONGODB_USERNAME}/$MONGODB_USERNAME/g" src/main/resources/config/employee-service.yaml
 sed -i "s/\${MONGODB_PASSWORD}/$MONGODB_PASSWORD/g" src/main/resources/config/employee-service.yaml
-
-# Display Department service yaml with passwords
-echo "Department Service YAML with passwords:"
-cat src/main/resources/config/department-service.yaml
-
-# Display Employee service yaml with passwords
-echo "Employee Service YAML with passwords:"
-cat src/main/resources/config/employee-service.yaml
